4598562a5b98e0fe936a9ac33872698b2e5bdfd6
Kooooojun <edisonhuang1122@gmail.com>
2022-06-05 22:23:27 +0800

    0605-22

---
+++ script.js
@@ -0,0 +1,34 @@
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+  if(window.scrollY > nav.offsetHeight + 25){
+    nav.classList.add('active')
+  }
+  else{
+    nav.classList.remove('active')
+  }
+}
+
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'TESTING!!'
+let idx = 1
+let speed = 300 / speedEl.value
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if(idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+}
+
+
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
